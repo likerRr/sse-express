98be2995dc4c694fe55f7824c44c9712c7b07018
Romain Lalaut <romain@quarkstudio.fr>
2017-05-18 17:41:29 +0200

    Clear interval when response ends

--- index.js
+++ index.js
@@ -15,9 +15,12 @@ function sseMiddleware(__, res, next) {
     'Access-Control-Allow-Origin': '*'
   });
 
-  setInterval(() => {
+  const handshakeInterval = setInterval(() => {
     res.write(': sse-handshake');
   }, 3000);
+  res.on('end', () => {
+    clearInterval(handshakeInterval);
+  });
 
   /**
    * Add function to response which allow to send events to the client
