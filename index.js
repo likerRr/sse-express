f3ff68f71c26009ee0450278ebdb8724a54bca3a
Alexey Lizurchik <al.lizurchik@gmail.com>
2017-06-03 18:58:50 +0300

    Split code to functions

--- index.js
+++ index.js
@@ -2,24 +2,20 @@ module.exports = exports = sseMiddleware;
 
 /**
  * Middleware that adds support of Server Sent Events
- * @param __
+ * @param req
  * @param res
  * @param next
  * @void
  */
-function sseMiddleware(__, res, next) {
+function sseMiddleware(req, res, next) {
   res.writeHead(200, {
     'Content-Type': 'text/event-stream',
     'Cache-Control': 'no-cache',
     'Connection': 'keep-alive'
   });
 
-  const handshakeInterval = setInterval(() => {
-    res.write(': sse-handshake');
-  }, 3000);
+  keepAlive(res, 3000);
 
-  res.on('finish', () => clearInterval(handshakeInterval));
-  res.on('close', () => clearInterval(handshakeInterval));
   res.sse = sendSse;
 
   next();
@@ -45,4 +41,16 @@ function sendSse(evt, json, id) {
   }
   res.write(`retry: 3000\n`);
   res.write(`data: ${JSON.stringify(json)}\n\n`);
+}
+
+/**
+ * Periodically sends messages to client to keep connection alive
+ * @param res
+ * @param repeatMs
+ */
+function keepAlive(res, repeatMs) {
+  const handshakeInterval = setInterval(() => res.write(': sse-handshake'), repeatMs);
+
+  res.on('finish', clearInterval(handshakeInterval));
+  res.on('close', clearInterval(handshakeInterval));
 }
